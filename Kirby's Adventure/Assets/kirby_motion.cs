6564ac288116ac38771a37f6a540e566a9629c30
weiligu <wegu@umich.edu>
2014-01-22 22:17:42 -0500

    first scene
    first scene with right move kirby + few collisions

---
+++ Kirby's Adventure/Assets/kirby_motion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kirby_motion : MonoBehaviour {
+
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var vertical = Input.GetAxis ("Vertical");
+		var horizontal = Input.GetAxis ("Horizontal");
+
+		if (horizontal > 0) {
+			animator.SetBool("Direction", true);
+			transform.position += transform.right * Time.deltaTime* 0.6f;
+		} else if (horizontal < 0) {
+			animator.SetBool("Direction", false);
+			transform.position -= transform.right * Time.deltaTime * 0.6f;
+		} else {
+			animator.SetBool("Direction", false);
+		}
+	}
+}

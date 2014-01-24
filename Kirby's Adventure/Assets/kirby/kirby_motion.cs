b778e473dda140bbcb9c79738f0cdc073441043e
yjyum <oreo_yjy@hotmail.com>
2014-01-24 16:22:26 -0500

    kirby's basic motions

--- Kirby's Adventure/Assets/kirby/kirby_motion.cs
+++ Kirby's Adventure/Assets/kirby/kirby_motion.cs
@@ -13,19 +13,50 @@ public class kirby_motion : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var horizontal = Input.GetAxis ("Horizontal");
+		var vertical = Input.GetAxis ("Vertical");
 
+		// horizontal move
 		if (horizontal > 0) {
 			transform.localScale = new Vector3(1f,1f,1f);
 			animator.SetBool("Direction", true);
-			//move kirby
+			//move kirby right
 			transform.position += transform.right * Time.deltaTime* 0.8f;
 		} else if (horizontal < 0) {
 			transform.localScale = new Vector3(-1f,1f,-1f);
 			animator.SetBool("Direction", true);
-			//move kirby
+			//move kirby left
 			transform.position -= transform.right * Time.deltaTime * 0.8f;
-		} else {
+		} else{
 			animator.SetBool("Direction", false);
+		}
+
+		// vertical move
+		if (vertical > 0 ) {
+			transform.rigidbody2D.mass = 0.3f;
+			animator.SetBool("withAir", true);
+			transform.position += transform.up * Time.deltaTime * 2.5f;
+		} 
+
+		// exhale or inhale air
+		if (Input.GetKeyDown (KeyCode.X)) {
+			if (animator.GetBool("withAir")) {
+				animator.SetBool("withAir", false);
+			} else {
+				animator.SetBool("inhale", true);
+			}
+		} else if (Input.GetKeyUp (KeyCode.X)) {
+			animator.SetBool("inhale", false);
+		}
+
+		// jump
+		if (Input.GetKey (KeyCode.Z)) {
+			if (transform.position.y < 2.5f) {
+				transform.position += transform.up * Time.deltaTime * 2.5f;
+			}
+		} else if (Input.GetKeyUp (KeyCode.Z)) {
+			animator.SetBool("jump", true);
+		} else {
+			animator.SetBool("jump", false);
 		}
 	}
 }

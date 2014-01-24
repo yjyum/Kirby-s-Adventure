cf3496ce2e49baf2c0ff62a3ea03b7e655e10d25
weiligu <wegu@umich.edu>
2014-01-23 20:52:11 -0500

    xyz
    xyz hell

---
+++ Kirby's Adventure/Assets/kirby_motion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kirby_motion : MonoBehaviour {
+
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var vertical = Input.GetAxis ("Vertical");
+		var horizontal = Input.GetAxis ("Horizontal");
+
+		if (horizontal > 0) {
+			animator.SetInteger("Direction", 1);
+			transform.position += transform.right * Time.deltaTime* 0.6f;
+		} else if (horizontal < 0) {
+			animator.SetInteger("Direction", 2);
+			transform.position -= transform.right * Time.deltaTime * 0.6f;
+		} else {
+			animator.SetInteger("Direction", 0);
+		}
+	}
+}

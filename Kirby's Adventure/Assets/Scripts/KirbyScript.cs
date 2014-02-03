99f00b85ee6be41c41e14b5b3b564576d48fdb4d
yjyum <oreo_yjy@hotmail.com>
2014-02-03 16:13:58 -0500

    update haha

--- Kirby's Adventure/Assets/Scripts/KirbyScript.cs
+++ Kirby's Adventure/Assets/Scripts/KirbyScript.cs
@@ -3,17 +3,21 @@ using System.Collections;
 
 public class KirbyScript : MonoBehaviour {
 
-	public float		walkSpeed = 6f;
-	public float 		dashSpeed = 7.2f;
-	public float		flySpeed = 4.4f;
-	public float		fallSpeed = 3.2f;
-	public float   	 	horSpeed = 6f;
-	public float		jumpSpeed = 8f;
-	public float		jumpAcc = 13f;
+	public float		walkSpeed = 2.5f;
+	public float 		dashSpeed = 3f;
+	public float 		slideSpeed = 1.7f;
+	public float		flySpeed = 2.3f;
+	public float		fallSpeed = 1.7f;
+	public float   	 	horSpeed = 2.5f;
+	public float		jumpSpeed = 6.5f;
+	public float		jumpAcc = 0.5f;
 
 	public float 		kirbyScale = 2.5f;
-	public float 		normalGravity = 2.8f;
-	public float 		flyGravity = 0.4f;
+	public float 		normalGravity = 1f;
+	public float 		flyGravity = 0.2f;
+
+	public static float kirbyAttackDis = 1.5f;
+
 	
 	public Vector2 		vel;
 	
@@ -24,7 +28,9 @@ public class KirbyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		animator = this.GetComponent<Animator>();
-		SingletonScript.Instance.kirby_animator = animator;
+		if (SingletonScript.Instance) {
+			SingletonScript.Instance.kirby_animator = animator;
+		}
 	}
 
 	// Update is called once per frame
@@ -43,9 +49,7 @@ public class KirbyScript : MonoBehaviour {
 			rigidbody2D.gravityScale = normalGravity;
 			horSpeed = walkSpeed;
 		}
-		
-		
-		
+
 		// "LEFT" & "RIGHT" command: walk left and right
 		if (!animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_duck")
 		    && !animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_inhale")) {
@@ -64,10 +68,11 @@ public class KirbyScript : MonoBehaviour {
 		
 		// "UP" command: Take in air and float in the air
 		//				 Flap arms to fly higher while floating
-		//				 Pass through a doorfly TODO
+		//				 Pass through a doorfly 
 		if (Input.GetKey (KeyCode.UpArrow)
 		    && !animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_duck")
-		    && !animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_inhale")) {
+		    && !animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_inhale")
+		    && !changeScene.atDoor) {
 			rigidbody2D.gravityScale = flyGravity;
 			vel.y = vertical * flySpeed;		
 			animator.SetBool ("withAir", true);
@@ -77,13 +82,20 @@ public class KirbyScript : MonoBehaviour {
 		}
 		
 		// "DOWN" command: Duck when standing still
-		//				   Swallow current item in Kirby's mouth TODO
+		//				   Swallow current item in Kirby's mouth 
+		if (Input.GetKeyDown (KeyCode.DownArrow) 
+		    && animator.GetBool("withEnemy")) { // swallow
+			animator.SetBool("swallow", true);
+		}
 		if (Input.GetKey (KeyCode.DownArrow)
-		    && !animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_inhale")) {
+		    && !animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_inhale")) { // duck
 			vel.x = 0;
 			animator.SetBool("duck", true);
 		} else {
 			animator.SetBool("duck", false);
+		}
+		if (Input.GetKeyUp (KeyCode.DownArrow)) { // swallow
+			animator.SetBool("swallow", false);
 		}
 		
 		// "Z" command: jump
@@ -110,6 +122,12 @@ public class KirbyScript : MonoBehaviour {
 				animator.SetBool("jump", false);
 			}
 		}
+		if (Input.GetKeyUp (KeyCode.Z)
+		    && animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_jump")) {
+			if (!grounded && vel.y > 0) {
+				vel.y = 0f;
+			}
+		}
 		
 		// "X" command: inhale when Kirby doesn't have a copy ability
 		//              Shoot out a star when an item is in Kirby's mouth TODO
@@ -117,17 +135,27 @@ public class KirbyScript : MonoBehaviour {
 		//              Puff attack while floating
 		if (Input.GetKeyDown (KeyCode.X)
 		    && !animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_duck")) {
-			if (animator.GetBool("withAir")) {
-				if (SingletonScript.Instance.current_enemy) {
-					if ((SingletonScript.Instance.current_enemy.transform.position - transform.position).magnitude <= 2) {
-						Destroy(SingletonScript.Instance.current_enemy);
-					}
+			if (animator.GetBool("withEnemy")) { // shoot star
+				animator.SetBool("withEnemy", false);
+				shootStar();
+			}
+			if (animator.GetBool("withAir")) { // puff attack
+				if (EnemyInDis(kirbyAttackDis)) {
+					Destroy(SingletonScript.Instance.current_enemy);
 				}
 				animator.SetBool("withAir", false);
 			} else {
 				animator.SetBool("inhale", true);
 			}
 		} 
+
+		if (Input.GetKey (KeyCode.X) && animator.GetBool("inhale")) { // inhale
+			if (EnemyInDis(kirbyAttackDis)) {
+				Debug.Log (this + "inhale enemy");
+				Destroy(SingletonScript.Instance.current_enemy);
+				animator.SetBool ("withEnemy", true);
+			}
+		}
 		if (Input.GetKeyUp (KeyCode.X)) {
 			animator.SetBool("inhale", false);
 		}
@@ -150,7 +178,7 @@ public class KirbyScript : MonoBehaviour {
 			}
 		}
 		if (animator.GetCurrentAnimatorStateInfo(0).IsName("kirby_slideAttack")) {
-			vel.x = transform.localScale.x * dashSpeed;
+			vel.x = transform.localScale.x * slideSpeed;
 		}
 		
 		rigidbody2D.velocity = vel;
@@ -163,6 +191,7 @@ public class KirbyScript : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		grounded = true;
 	}
+
 	void OnTriggerExit2D(Collider2D other) {
 		grounded = false;
 	}
@@ -170,8 +199,30 @@ public class KirbyScript : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D col) {
 		// kirby died
 		if (col.gameObject.tag.Equals("Enemy")) {
-			Debug.Log("collision kirby");
+			Debug.Log(this + "collision kirby");
 			Application.LoadLevel ("Vegetable Valley 1");
 		}
+	}
+
+
+	// helper func
+	GameObject EnemyInDis(float distance) {
+		if (SingletonScript.Instance) {
+			if (SingletonScript.Instance.current_enemy) {
+				if ((SingletonScript.Instance.current_enemy.transform.position 
+				     - transform.position).magnitude <= distance) {
+					return SingletonScript.Instance.current_enemy;
+				}
+			}
+		}
+		return null;
+	}
+
+	void shootStar() {
+		GameObject star = GameObject.Instantiate (GameObject.Find("shootStar"), 
+		                                          transform.position, 
+		                                          Quaternion.identity) as GameObject;
+		star.rigidbody2D.AddForce (transform.forward * 2);
+
 	}
 }

80c1b8ec0454bf4baa641839db72a01c0a29c6f1
yjyum <oreo_yjy@hotmail.com>
2014-02-08 23:47:55 -0500

    with scoreboard

--- Kirby's Adventure/Assets/Scripts/MoveScoreScript.cs
+++ Kirby's Adventure/Assets/Scripts/MoveScoreScript.cs
@@ -11,8 +11,9 @@ public class MoveScoreScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 pos = Camera.main.transform.position;
-		pos.y -= 3.5f;
+		pos.y -= 4f;
 		pos.x -= 1.4f;
+		pos.z = 0f;
 		transform.position = pos;
 	}
 }

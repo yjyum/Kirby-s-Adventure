bf272339154d861bd46c4d98908cf594ce5d5727
yjyum <oreo_yjy@hotmail.com>
2014-02-09 04:24:35 -0500

    +bronto burt

--- Kirby's Adventure/Assets/Scripts/MoveScript.cs
+++ Kirby's Adventure/Assets/Scripts/MoveScript.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MoveScript : MonoBehaviour {
 
 	public float		speed = -1.5f;
+	public float 		flySpeed = 4f;
+	public float		flyTime = 0.8f;
+	public bool 		canFly = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +17,18 @@ public class MoveScript : MonoBehaviour {
 	void Update () {
 		Vector2 vel = rigidbody2D.velocity;
 		vel.x = speed;
+		if (canFly) {
+			flyTime -= Time.deltaTime;
+			if (flyTime <= 0) {
+				flySpeed *= Random.Range (-1, 1);
+				flyTime = 0.8f;
+			} 
+			if (transform.position.y <= 8f) {
+				flySpeed = 4f;
+				flyTime = 0.8f;
+			}
+			vel.y = flySpeed;
+		}
 		rigidbody2D.velocity = vel;
 	}
 
